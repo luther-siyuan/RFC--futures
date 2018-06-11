cb15eb58e168e0672c4453908acd834d67673a3c
liusiyuan-win <1376030681@qq.com>
2018-06-11 20:17:41 +0800

    将RFC运用于期货风控

---
+++ cmake-build-debug/CMakeFiles/Demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Demo.dir/RFC.CPP.o"
+  "Demo.pdb"
+  "Demo.exe"
+  "libDemo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/rfc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rfc.dir/RFC.CPP.o"
+  "rfc.pdb"
+  "rfc.exe"
+  "librfc.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rfc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
